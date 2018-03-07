c747a7c0ce1c3f7561ad8bcfd768473c04202a18
Joaquim Rosa <joaquimacrosa@outlook.com>
2018-03-07 12:38:00 +0000

    contact.php redesigned
    Did a reset on the front-end of the contact page

--- contact.php
+++ contact.php
@@ -28,69 +28,50 @@ include 'library/template_functions/t_functions.php';
 
 <div id="wrapper">
 
-  <main id="contact-page">
-    <div id="fullpage" class="fullpage-wrapper">
+    <main id="contact-page">
+        <div class="contactpage-header"><h1 class="white">Contact Us</h1></div>
 
-        <div class="section fp-section fp-table fp-completely" id="section0" data-anchor="OurFounders">
-          <div class="page-header contact-header"><h1>Contact Us</h1></div>
-          <div class="page-content">
-          <!--SECTION 1-->
-           <div id="contactsSection1">
-                <a href="#" id="mailContacts">info@gifdesignstudios.com</a><br>
-                <span>Praceta Adelino Amaro da Costa, 728, 8E</span><br>
-                <span>Porto -- Portugal</span><br><br>
+        <div class="fullpage-wrapper">
+
+            <div id="contact-adress">
+                <a href="mailto:info@gifdesignstudios.com"><p>info@gifdesignstudios.com</p></a>
+                <p>Praceta Adelino Amaro da Costa, 728, 8E</p>
+                <p>Porto -- Portugal</p>
             </div>
-           <!--END SECTION 1-->
-           <!--SECTION 2-->
-           <div id="contactsSection2">
+
+            <div id="contacts-form">
                 <p>Dear GIF Design Studios,</p>
                 <form id="formContacts">
-                  <div>
-                      <textarea id="textarea-contact" class="contact-us" name="message"></textarea>
-                  </div>
+                <div id="message-gif">
+                    <textarea name="message"></textarea>
+                </div>
 
-            </div>
-           <!--END SECTION 2-->
-           <!--SECTION 3-->
-
-           <!-- <div id="contactsSection3">
-                <p>Dear GIF Design Studios,</p>
-
-                  <div >
-                      <span id="spanContactSec3" name="message"></span>
-                  </div>
-            </div> -->
-
-           <!--END SECTION 3-->
-           <!--SECTION 4-->
-           <div id="contactsSection4" >
                 <p>We just need your email so we can get back to you:</p>
-
                  <label>
-                  <div>
-                      <input id="contactsInputMail" type="email" name="email" >
-                      <div style="display:none;color:red;margin-top:20px;" class="message-error">*Insert your message</div>
-                      <div style="display:none;color:red;" class="email-error">*Insert your email</div>
+                     <div>
+                          <input type="email" name="email" >
+                          <div style="display:none;color:red;margin-top:20px;" class="message-error">*Insert your message</div>
+                          <div style="display:none;color:red;" class="email-error">*Insert your email</div>
                      </div>
                 </label>
                 <label>
-                <input style="margin-right: 14px; margin-top: -7px; float: left;display:inline-block;outline: none;" id="contactSendMailBtn" class="fromLeft btn btn-white-orange" type="submit" value="Send Message" />
-                <input type="hidden" name="button_pressed" value="1" />
-                <div class="s-message" style=" display:none;  margin-left: 20px;text-transform:uppercase;    font-family: Cera-Regular, Helvetica, Arial, sans-serif;">thank you for your contact. We'll get back to you shortly.</div>
+                    <input id="contactSendMailBtn" class="fromLeft btn btn-white-orange" type="submit" value="Send Message" />
+                    <input type="hidden" name="button_pressed" value="1" />
+                    <div class="s-message" style=" display:none;  margin-left: 20px;text-transform:uppercase;    font-family: Cera-Regular, Helvetica, Arial, sans-serif;">thank you for your contact. We'll get back to you shortly.</div>
                 </label>
+
                 </form>
 
             </div>
-           <!--END SECTION 4-->
-          </div>
         </div>
+    </div>
 
 
     </div>
 
 </main>
 </div>
-<? echo getFooter(); ?>
+<?php include ('shared/footer-contact.php');?>
 <script src="js/jquery.imagesloaded.min.js"></script>
 
 
---
+++ shared/footer-contact.php
@@ -0,0 +1,33 @@
+<!--FOOTER-->
+
+<footer class="pages-footer-contact">
+    <div class="footer-container">
+        <div class="col-4 footer-text">
+            <div class="vertical-center">
+                <div>
+                    Spreading magic from <span class="bold">Porto</span> to the world.
+                </div>
+            </div>
+        </div>
+        <div class="col-4 align-center footer-social-row">
+            <div class="vertical-center">
+                <div class="social-icons">
+                    <a target="_blank" href="http://www.behance.net/gifdesignstudios"><i aria-hidden="true" class="fa fa-behance"></i></a><a target="_blank" href="http://www.facebook.com/gifdesignstudios">
+                      <i aria-hidden="true" class="fa fa-facebook"></i>
+                    </a>
+                    <a target="_blank" href="http://www.instagram.com/gifdesignstudios">
+                      <i aria-hidden="true" class="fa fa-instagram"></i>
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/company/gif-design-studios">
+                      <i aria-hidden="true" class="fa fa-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 desktop">
+            <a class="btn btn-orange dektop" href="/hire-us">Hire Us!</a>
+        </div>
+    </div>
+</footer>
+
+  <!--END FOOTER-->
